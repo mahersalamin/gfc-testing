af59cfa929a8ba52ab00dda1916dc13dc14fb312
Maher Salamin <salaminmaher@gmail.com>
2022-01-18 08:14:45 +0200

    first

---
+++ database/migrations/2021_12_07_201223_create_like_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('like', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userId');
+            $table->unsignedBigInteger('giftId');
+            $table->foreign('userId')->references('id')->on('users');
+            $table->foreign('giftId')->references('id')->on('gifts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('like');
+    }
+}
